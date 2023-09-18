ae44a8443ee7de2cbced68dfcc50e84304f2ca07
mamun-prodhan <mamunpr9@gmail.com>
2023-09-18 15:05:02 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <h1>Price Options</h1>
+      <button className="btn btn-info">Info</button>
+      <button className="btn btn-success">Success</button>
+      <button className="btn btn-warning">Warning</button>
+      <button className="btn btn-error">Error</button>
+    </>
+  );
+}
+
+export default App;
