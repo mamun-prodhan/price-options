d4ececc9a8033463678db3af9138599baf739d51
mamun-prodhan <mamunpr9@gmail.com>
2023-09-20 12:38:39 +0600

    theme added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
 import { useState } from "react";
 import NavBar from "./components/NavBar/NavBar";
 import PriceOptions from "./components/PriceOptions/PriceOptions";
+import { BsFillSunFill, BsFillMoonStarsFill } from "react-icons/bs";
 
 function App() {
   const [dark, setDark] = useState(false);
   return (
     <>
       <button
-        className={`ms-6  px-4 py-2 text-xl font-extrabold ${
+        className={`ms-6  px-4 rounded-lg py-2 text-xl font-extrabold ${
           dark ? "text-white bg-[#000]" : "text-[#000] bg-yellow-400"
         }`}
         onClick={() => setDark(!dark)}
       >
-        {dark ? "Light" : "Dark"}
+        {dark ? (
+          <div className="flex gap-2 items-center">
+            Light<BsFillSunFill></BsFillSunFill>
+          </div>
+        ) : (
+          <div className="flex gap-2 items-center">
+            Dark
+            <BsFillMoonStarsFill></BsFillMoonStarsFill>
+          </div>
+        )}
       </button>
       <NavBar></NavBar>
       <PriceOptions dark={dark}></PriceOptions>
